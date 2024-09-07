0d9c75d70d5fb500af4c1d23816a6c790b530d93
psakharov <s.p.alekseevich@gmail.com>
2024-09-07 16:54:38 +0300

    add relative data transformer

--- src/Relative/RelativeData.php
+++ src/Relative/RelativeData.php
@@ -4,5 +4,75 @@ namespace Glider88\RelativeCoordinates\Relative;
 
 class RelativeData
 {
+    private RelativeCoordinates $transformer;
+    private string $startEscape;
+    private string $endEscape;
 
+    public function __construct(RelativeCoordinates $transformer, array $escapes = ['{{', '}}'])
+    {
+        if (count($escapes) === 1) {
+            $escapes = [$escapes[0], $escapes[0]];
+        }
+
+        [$start, $end] = $escapes;
+
+        $this->transformer = $transformer;
+        $this->startEscape = $start;
+        $this->endEscape = $end;
+    }
+
+    /**
+     * @param string[] | string[][] $relData
+     * @return string[] | string[][]
+     */
+    public function absolute(array $relData): array
+    {
+        $applyFn = function ($args) {
+            if (is_array($args)) {
+                return array_map(fn($arg) => $this->absoluteData($arg), $args);
+            }
+
+            return $this->absoluteData($args);
+        };
+
+        return array_map($applyFn, $relData);
+    }
+
+    /**
+     * @param string[]|string|mixed $cellData
+     * @return string[]|string|mixed
+     */
+    private function absoluteData($cellData)
+    {
+        if (!is_string($cellData)) {
+            return $cellData;
+        }
+
+        $parts = explode($this->startEscape, $cellData);
+        $prefix = array_shift($parts);
+
+        $fn = function ($arg) use ($cellData) {
+            $relAndStr = explode($this->endEscape, $arg);
+            if (count($relAndStr) === 1) {
+                array_unshift($relAndStr, '');
+            }
+            [$rel, $str] = $relAndStr;
+
+            return $this->transformer->absolute($rel) . $str;
+        };
+
+        $absParts = array_map($fn, $parts);
+
+        return $prefix . implode('', $absParts);
+    }
+
+    private function height(array $data): int
+    {
+        $firstRow = current($data);
+        if (!is_array($firstRow)) {
+            return 1;
+        }
+
+        return count($data);
+    }
 }
--- tests/Relative/RelativeDataTest.php
+++ tests/Relative/RelativeDataTest.php
@@ -2,37 +2,54 @@
 
 namespace Tests\Glider88\RelativeCoordinates\Relative;
 
-use Glider88\RelativeCoordinates\Relative\RelativeDate;
+use Glider88\RelativeCoordinates\Relative\RelativeCoordinates;
+use Glider88\RelativeCoordinates\Relative\RelativeData;
 use PHPUnit\Framework\Attributes\DataProvider;
-use PHPUnit\Framework\Attributes\TestWith;
 use PHPUnit\Framework\TestCase;
 
-class RelativeDateTest extends TestCase
+class RelativeDataTest extends TestCase
 {
     public static function absoluteProvider(): array
     {
-        // A  B  C  D  E  F  G  H  I  J  K  L
+        // A    B    C      D     E     F   G
+        //    one  two  three  four  five
+
         // 1  2  3  4  5  6  7  8  9
-        $titles = ['one', 'two', 'three', 'four', 'five'];
+        //       1  2  3  4  5  6  7
+
+        $cs = ['one', 'two', 'three', 'four', 'five'];
+
         return [
-            '' => [],
+            'only column' =>
+                [$cs, ['[three]'], ['D']],
+            'only row' =>
+                [$cs, ['[2]'], ['4']],
+            'cell' =>
+                [$cs, ['[four4]'], ['E6']],
+            'range' =>
+                [$cs, ['[one1:five4]'], ['B3:F6']],
+            'multiple rows' =>
+                [
+                    $cs,
+                    ['row1' => ['[one1:two2]', '[one1:two2]'], 'row2' => ['[three2:five5]', '[three2:five5]']],
+                    ['row1' => ['B3:C4', 'B3:C4'], 'row2' => ['D4:F7', 'D4:F7']],
+                ],
+            'different escapes position' =>
+                [$cs, ['[two][1][:four4]'], ['C3:E6']],
+            'with other words' =>
+                [$cs, ['one1[three3]4four5'], ['one1D54four5']],
+            'default columns' =>
+                [[], ['[C7]'], ['D9']],
         ];
     }
 
     #[DataProvider('absoluteProvider')]
-    public function testAbsolute(string $start, array $titles, string $relCoord, string $expected): void
+    public function testAbsolute(array $columns, array $relData, array $expected): void
     {
-        $coordinateT = RelativeDate::generate($start, $titles);
-        $actual = $coordinateT->absolute($relCoord);
+        $coordinateT = RelativeCoordinates::new('B3', $columns);
+        $dataT = new RelativeData($coordinateT, ['[', ']']);
+        $actual = $dataT->absolute($relData);
 
         $this->assertSame($expected, $actual);
-    }
-
-    #[TestWith([], '')]
-    public function testAbsoluteFailed(array $titles, string $relCoord): void
-    {
-        $coordinateT = RelativeDate::generate('A1', $titles);
-        $this->expectException(\InvalidArgumentException::class);
-        $coordinateT->absolute($relCoord);
     }
 }
